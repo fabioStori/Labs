3f595701ef87418fbdc65f1a48999e061e1b4d3b
Fabio Stori <33457554+fabioStori@users.noreply.github.com>
2017-11-07 14:06:09 -0200

    Lab07.c - Esqueleto

--- Lab07.c
+++ Lab07.c
@@ -1,1 +1,50 @@
+/*
+  Lab07: Painel de Controle de Programas
+  Nome: Fabio Stori
+  RA: 196631
+  Nome: Adivair Santana Ramos
+  RA: 193325
+  Turma E - MC202
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+typedef struct No{
+
+}No;
+
+int  main(){
+  int op;
+ 
+  while(scanf("%d", &op)!= EOF){    //ate o arquivo acabar...
+    
+    switch(op){
+     
+    case(1):
+    break;
+     
+    case(2):
+    break;
+     
+    case(3): 
+    break;
+     
+    case(4):           
+    break;
+    
+    case(5):
+    break;
+
+    case(6):
+    break;
+
+    case(7):
+    break;
+    }
+  }
+  printf("\n");
+  return 0;
+}
 
