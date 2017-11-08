75ef64e6c0b6eeede40a013a61583b527f93ee3d
Fabio Stori <33457554+fabioStori@users.noreply.github.com>
2017-11-08 12:47:09 -0200

    Só uns scanfs de diferença

--- Lab07.c
+++ Lab07.c
@@ -10,38 +10,45 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-
 typedef struct No{
 
 }No;
 
 int  main(){
-  int op;
- 
+  int op, P, i;
+  
+  scanf("%d", &P);
+  
+  char in-ordem[P][30], pre-ordem[P][30];
+  
+  for(i=0;i<P;i++)
+    scanf("%s", in-ordem[i]);
+  for(i=0;i<P;i++)
+    scanf("%s", pre-ordem[i]);
+  
   while(scanf("%d", &op)!= EOF){    //ate o arquivo acabar...
     
-    switch(op){
-     
-    case(1):
-    break;
-     
-    case(2):
-    break;
-     
-    case(3): 
-    break;
-     
-    case(4):           
-    break;
-    
-    case(5):
-    break;
+    switch(op){     
+      case(1):
+      break;
 
-    case(6):
-    break;
+      case(2):
+      break;
 
-    case(7):
-    break;
+      case(3): 
+      break;
+
+      case(4):           
+      break;
+
+      case(5):
+      break;
+
+      case(6):
+      break;
+
+      case(7):
+      break;
     }
   }
   printf("\n");
