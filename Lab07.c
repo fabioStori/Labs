0dcc981fb93f2be678d68a4c62dc0040ef705abe
Fabio Stori <33457554+fabioStori@users.noreply.github.com>
2017-11-08 14:12:19 -0200

    Inserir 0.15

--- Lab07.c
+++ Lab07.c
@@ -21,12 +21,17 @@ void inserir (Pasta *pasta, char programa[30]);
 
 void alocar (Pasta *pasta, char programa[30]);
 
+void imprimeInOrdem(Pasta *pasta);
+
+void inicializaRaiz(Pasta *raiz);
+
 int  main(){
   int op, P, i;
-  
+  Pasta *raiz = malloc(sizeof(Pasta));
+  inicializaRaiz(raiz);
   scanf("%d", &P);
   
-  char in-ordem[P][30], pre-ordem[P][30];
+  char in-ordem[P][30], pre-ordem[P][30], programa[30];
   
   for(i=0;i<P;i++)
     scanf("%s", in-ordem[i]);
@@ -37,7 +42,8 @@ int  main(){
     
     switch(op){     
       case(1):
-        inserir();
+        scanf("%s", programa);
+        inserir(raiz, programa);    //raiz sera o no inicial que eu ainda nao criei nessa versao
       break;
 
       case(2):
@@ -56,13 +62,18 @@ int  main(){
       break;
 
       case(7):
+        imprimeInOrdem(raiz);
       break;
     }
   }
   printf("\n");
   return 0;
 }
-
+void inicializaRaiz(Pasta *raiz){
+  raiz->pai = NULL;
+  raiz->dir = NULL;
+  raiz->esq = NULL;
+}
 void inserir (Pasta *pasta, char programa[30]){
   if(pasta){
     int teste = strcmp(programa, pasta->programa);
@@ -82,3 +93,11 @@ void alocar (Pasta *pasta, char programa[30]){
   novo->dir = NULL;
   novo->esq = NULL;
 }
+void imprimeInOrdem(Pasta *pasta){
+  if(pasta){
+    imprimeInOrdem(pasta->esq);
+    printf("Programa: %s", pasta->programa);
+    imprimeInOrdem(pasta->dir);
+  }
+}
+
