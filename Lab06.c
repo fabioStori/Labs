34f4a72d6839852a0fa8861197d67d99ce77b36d
AdivairRamos <33488482+AdivairRamos@users.noreply.github.com>
2017-11-11 16:08:56 -0200

    Update Lab06.c

--- Lab06.c
+++ Lab06.c
@@ -1,308 +1,346 @@
 /*
-  Lab06: Gerenciador de Memória
+  Lab07: Painel de Controle de Programas
   Nome: Fabio Stori
   RA: 196631
-  Nome: Adivair Santana Ramos
+  Nome: Adivair Satana Ramos
   RA: 193325
   Turma E - MC202
 */
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include <math.h>
 
-typedef enum {Livre, Ocupado, Particionado} Estado;
+int NUM_PROG;
 
-typedef struct No{
-  int cod;
-  int size;
-  int size_max;
-  struct No* pai;
-  struct No* esq;
-  struct No* dir;
-  Estado estado;
-}No;
+typedef enum {Esq, Dir, Raiz} Filho;
 
-void inicializa_Raiz(No *raiz, int exp);
+typedef struct Pasta{
+    struct Pasta *dir;
+    struct Pasta *esq;
+    struct Pasta *pai;
+    char programa[30];
+    char nome[34];
+}Pasta;
 
-int inserir_no(No *arvore, int cod, int size, int exp);
-//percorre em pre-ordem ate encontrar um no livre e com espaço suficiente, insere ou cria dois filhos
+Pasta* inserir (Pasta *pasta, char programa[30]);
 
-void criarFilhos(No *arvore, int exp);
-//dado um No, cria dois filhos com espaço 2^exp;
+void alocarPasta (Pasta *pasta, char programa[30], Filho filho);
 
-int remover (No *arvore, int cod);
-//remove um no especifico dado o Cod;
+void imprimeInOrdem(Pasta *pasta);
 
-void removerPai(No *arvore);
+void imprimePreOrdem(Pasta *pasta);
 
-void calcula_Frag(No *arvore, int *frag);
-//percorre toda arvore somando em uma acumuladora o valor de memoria desperdiçada
+void inicializaRaiz(Pasta *raiz);
 
-int imprimeOcupados(No *arvore, int i);
-//imprime os Nos folhas ocupados
+char **alocaMatriz(int lin, int col);
 
-void relatorio(No *arvore);
-//percorre a arvore recursivamente, acumulando os valores de memoria usados e a quantidade de cada tipo de Nós
+Pasta *constrArv(char **inOrdem, char **preOrdem, int l, int r, int *indPre, Pasta *pai, Filho filho);
 
-void calcula_Inf(No *arvore, int *p, int *o, int *l, int *u);
-//função de percorrer usada em relatorio
+int nivelPrograma(Pasta *raiz, char *busca, int nivel);
 
-void imprime_Sem(No *arvore);
-//imprime os Nós dado o percurso(in, pre, pos ordem)
-void imprime_Sim(No *arvore);
+Pasta* remover(Pasta *pasta, char programa[30]);
 
-void imprime_Pre(No *arvore);
+void substituirAntecessor(Pasta *pasta);
 
-void imprime_Pos(No *arvore);
+void testeVelocidade(int t_gasto, int t_max, char *prog);
 
-int pot_2(int exp);
+Pasta* balanceamento(Pasta *raiz);
 
-void desalocar(No *arvore);
+void extraiSemente(Pasta *raiz, char **aux, int *indice);
 
-int  main(){
-  int exp, op, cod, size, teste, frag;
-  
-  scanf("%d", &exp);    
-  No *raiz = malloc(sizeof(No));
-  inicializa_Raiz(raiz, exp);
-  
-  while(scanf("%d", &op)!= EOF){    //ate o arquivo acabar...
-    
-    switch(op){
-      
-    case(1):
-      scanf("%d %d", &cod, &size);
-      teste = inserir_no(raiz, cod, size, exp);
-      if(!teste)
-        printf("Memoria insuficiente\n");
-      break;
-      
-    case(2):
-      scanf("%d", &cod);
-      if(remover(raiz, cod))
-	printf("Processo (%d) finalizado com sucesso\n", cod);
-      else
-	printf("Nao existe processo de codigo %d inicializado no sistema\n", cod);
-      break;
-      
-    case(3): 
-      frag = 0;        
-      calcula_Frag(raiz, &frag);
-      printf("Quantidade total de memoria desperdicada por fragmentacao: %d\n", frag);
-      break;
-      
-    case(4):             
-      relatorio(raiz);
-      break;
-      
-    case(5):
-      imprime_Sem(raiz);
-      break;
-    case(6):
-      printf("[PROCESSOS PRESENTES NA MEMORIA]\n");
-      teste = imprimeOcupados(raiz, 1);
-    if(teste)
-      printf("Nenhum processo presente\n");
-    break;
-    }
-  }
-  printf("\n");
-  desalocar(raiz);
-  //free(raiz);
-  return 0;
-}
+Pasta *criaArv(char **semente, int l, int r, Filho filho, Pasta *pai);
 
+void desaloca(Pasta *raiz);
 
-void inicializa_Raiz(No *raiz, int exp){ //Inicicializa a raiz como um no livre, sem pai e filhos...
-  raiz->pai = NULL;
-  raiz->esq = NULL;
-  raiz->dir = NULL;
-  raiz->estado = Livre;
-  raiz->size = 0;
-  raiz->size_max = pot_2(exp); 
-}
+int  main() {
+    int op, P, i, tempo, nivel;
+    scanf("%d", &P);
+    Pasta *raiz;
+    NUM_PROG = P;
+    //= malloc(sizeof(Pasta));
+    //inicializaRaiz(raiz);
+    //raiz = NULL;
+    char **inOrdem, **preOrdem, programa[30];
+    inOrdem = alocaMatriz(P, 30);
+    preOrdem = alocaMatriz(P, 30);
+    for (i = 0; i < P; i++)
+        scanf("%s", inOrdem[i]);
+    for (i = 0; i < P; i++)
+        scanf("%s", preOrdem[i]);
+    int indPre = 0;
+    raiz = constrArv(inOrdem, preOrdem, 0, P, &indPre, NULL, Raiz);
 
-int inserir_no(No *arvore, int cod, int size, int exp){
-  if(arvore){
-    if((arvore->estado == Livre) && (size <= arvore->size_max)){// Se encontrar um livre que cabe, verifica as condiçoes de inserção ou fragmentação
-      if((size > arvore->size_max/2)){// Se o valor for maior que a metade do No, ocupa-o
-	arvore->cod = cod;
-	arvore->size = size;
-	arvore->estado = Ocupado;
-	printf("Processo (%d) de tamanho %d inicializado com sucesso\n", cod, size);
-	return 1;
-      }else{// Se o valor for menor que a metade do No, fragmenta-o
-	arvore->estado = Particionado;
-	criarFilhos(arvore, exp-1);                
-      }
-    }   
-    if(inserir_no(arvore->esq, cod, size, exp-1))
-      return  1;        
-    return inserir_no(arvore->dir, cod, size, exp-1);              
-  }
-  return 0;
-}
+    while (scanf("%d", &op) != EOF) {    //ate o arquivo acabar...
 
+        switch (op) {
+            case (1):
+                scanf("%s", programa);
+                raiz = inserir(raiz, programa);
+                NUM_PROG++;
+                break;
 
-void criarFilhos(No *arvore, int exp){
-  No *novo_1, *novo_2;
-  novo_1 = malloc(sizeof(No));
-  novo_2 = malloc(sizeof(No));
-  
-  novo_1->esq = NULL;             novo_2->esq = NULL;  
-  novo_1->dir = NULL;             novo_2->dir = NULL;
-  novo_1->pai = arvore;           novo_2->pai = arvore;
-  novo_1->size_max = pot_2(exp);   novo_2->size_max = pot_2(exp);
-  novo_1->estado = Livre;         novo_2->estado = Livre;
-  
-  arvore->esq = novo_1;           arvore->dir = novo_2;
-}
+            case (2):
+                scanf("%s", programa);
+                raiz = remover(raiz, programa);
+                NUM_PROG--;
+                break;
 
-int pot_2(int exp){
-  int total=1, i;
-  for(i=0; i < exp; i++)  
-    total = total*2;
-  return total;
-}
+            case (3):
+                scanf("%s %d", programa, &tempo);
+                testeVelocidade(nivelPrograma(raiz, programa, 0), tempo, programa);
+                break;
 
-int remover (No *arvore, int cod){
-  if(arvore){
-    if((arvore->estado == Ocupado) && (arvore->cod == cod)){
-            arvore->estado = Livre;
-            arvore->cod = 0;
-            arvore->size = 0;
-            return 1;
-    }
-    int teste = remover(arvore->esq, cod);
-    if(teste){
-      if((arvore->esq->estado == Livre) && (arvore->dir->estado == Livre)){
-	removerPai(arvore);
-      }
-      return 1;
-    }
-    teste = remover (arvore->dir, cod);
-        if(teste){
-	  if((arvore->esq->estado == Livre) && (arvore->dir->estado == Livre)){
-	    removerPai(arvore);
-	  }
-	  return 1;
+            case (4):
+                raiz = balanceamento(raiz);
+                break;
+
+            case (5):
+                break;
+
+            case (6):
+                break;
+
+            case (7):
+                imprimeInOrdem(raiz);
+                printf("Total de Prog: %d \n", NUM_PROG);
+                break;
         }
-        return 0;
-  }
-  return 0;
-}
-
-void removerPai(No *arvore){
-  arvore->estado = Livre;
-  arvore->cod = 0;
-  arvore->size = 0;
-  free(arvore->esq);
-  free(arvore->dir);
-  arvore->esq = NULL;
-  arvore->dir = NULL;
-}
-
-void calcula_Frag(No *arvore, int *frag){  
-  if(arvore){
-    if(arvore->estado == Ocupado){
-      (*frag) = (*frag) + (arvore->size_max - arvore->size);
     }
-    calcula_Frag(arvore->esq, frag);
-    calcula_Frag(arvore->dir, frag);
-  }
 }
-int imprimeOcupados(No *arvore, int i){
-  if(arvore){        
-    i= imprimeOcupados(arvore->esq, i);
-    if(arvore->estado == Ocupado){
-      printf("%d [Processo: %d]\n", arvore->size_max, arvore->cod);
-      i=0;
+
+
+void inicializaRaiz(Pasta *raiz) {
+    raiz->pai = NULL;
+    raiz->dir = NULL;
+    raiz->esq = NULL;
+}
+
+Pasta *remover(Pasta *pasta, char programa[30]) {
+    if (pasta == NULL)
+        return NULL;
+    if ((strcmp(pasta->nome, "raiz")) == 0 && pasta->esq == NULL &&
+        pasta->dir == NULL) {   //caso em que existe apenas a raiz sem filhos
+        free(pasta);
+        return NULL;
     }
-    i = imprimeOcupados(arvore->dir, i);
-  }
-  return i;
+    int teste = strcmp(programa, pasta->programa);
+    if (teste < 0) {
+        pasta->esq = remover(pasta->esq, programa);
+        return pasta;
+    } else if (teste > 0) {
+        pasta->dir = remover(pasta->dir, programa);
+        return pasta;
+    } else if (pasta->esq == NULL) {
+        return pasta->dir;
+    } else if (pasta->dir == NULL) {
+        return pasta->esq;
+    } else {
+        substituirAntecessor(pasta);
+        return pasta;
+    }
 }
 
-
-void calcula_Inf(No *arvore, int *p, int *o, int *l, int *u){
-  if(arvore){
-    if(arvore->estado == Ocupado){
-      (*o)++;
-      (*u) = (*u) + arvore->size_max;
-    }      
-    if(arvore->estado == Particionado)
-      (*p)++;
-    if(arvore->estado == Livre)
-      (*l)++;
-    calcula_Inf(arvore->esq, p, o, l, u);
-    calcula_Inf(arvore->dir, p, o, l, u);
-  }
-}
-
-void relatorio(No *arvore){
-  int p=0, o=0, l=0, u=0, max= arvore->size_max;
-  float por;  
-  printf("[RELATORIO DE SISTEMA]\n");
-  calcula_Inf(arvore, &p, &o, &l, &u);
-  por = (float) u / (float) (max)*100;
-  printf("%d Ocupados\n%d Livres\n%d Particionados\nMemoria utilizada = %d / 100\n", o, l, p, (int) por);
-}
-
-
-void imprime_Sem(No *arvore){
-  printf("[SEMENTES GERADORAS]\n");
-  printf("Sim = "); imprime_Sim(arvore); printf("\n");
-  printf("Pre = "); imprime_Pre(arvore); printf("\n");
-  printf("Pos = "); imprime_Pos(arvore); printf("\n");
-  
-}
-
-void imprime_Sim(No *arvore){
-  if(arvore){
-    imprime_Sim(arvore->esq);
-    if(arvore->estado == Livre)
-        printf("(L:%d)", arvore->size_max);
-    else if(arvore->estado == Particionado)
-      printf("(P:%d)", arvore->size_max);
+void substituirAntecessor(Pasta *pasta) {
+    Pasta *pai = pasta, *t = pasta->esq;
+    while (t->dir != NULL) {
+        pai = t;
+        t = t->dir;
+    }
+    if (pai->esq == t)
+        pai->esq = t->dir;
     else
-      printf("(O:%d/%d[%d])", arvore->size, arvore->size_max, arvore->cod);   
-    imprime_Sim(arvore->dir);
-  }
+        pai->dir = t->esq;
+    strcpy(pasta->programa, t->programa);
+    printf("Programa: %s, Pasta: %s | removidos\n", pasta->programa, pasta->nome);
+}
+
+Pasta *inserir(Pasta *pasta, char programa[30]) {
+    if (pasta) {
+        int teste = strcmp(programa, pasta->programa);    //testa se o programa e maior ou menor
+        if (teste < 0) {      //se for menor, vai pra esquerda
+            if (pasta->esq == NULL)    //e se o da esquerda for nulo, insere
+                alocarPasta(pasta, programa, Esq);
+            else
+                inserir(pasta->esq, programa);    //se nao, testa o da esquerda
+        } else {      //se nao for menor, é maior, logo executa um procedimento analogo para a direita
+            if (pasta->dir == NULL)
+                alocarPasta(pasta, programa, Dir);
+            else
+                inserir(pasta->dir, programa);
+        }
+        return pasta;
+    } else {    //caso em que a arvore esta vazia
+        Pasta *novo = malloc(sizeof(Pasta));
+        novo->dir = NULL;
+        novo->esq = NULL;
+        novo->pai = NULL;
+        strcpy(novo->nome, "raiz");
+        strcpy(novo->programa, programa);
+        return novo;
+    }
+}
+
+void alocarPasta(Pasta *pasta, char programa[30], Filho filho) {
+    Pasta *novo = malloc(sizeof(Pasta));
+    novo->dir = NULL;
+    novo->esq = NULL;
+    novo->pai = pasta;
+    strcpy(novo->nome, pasta->programa);
+    strcpy(novo->programa, programa);
+    if (filho == Esq) {
+        strcat(novo->nome, "_esq");
+        pasta->esq = novo;
+    } else if (filho == Dir) {
+        strcat(novo->nome, "_dir");
+        pasta->dir = novo;
+    }
+}
+
+void imprimeInOrdem(Pasta *pasta) {
+    if (pasta) {
+        imprimeInOrdem(pasta->esq);
+        printf("Programa: %s | Pasta: %s\n", pasta->programa, pasta->nome);
+        imprimeInOrdem(pasta->dir);
+    }
+}
+
+void imprimePreOrdem(Pasta *pasta) {
+    if (pasta) {
+        printf("Programa: %s | Pasta: %s\n", pasta->programa, pasta->nome);
+        imprimeInOrdem(pasta->esq);
+        imprimeInOrdem(pasta->dir);
+    }
+}
+
+char **alocaMatriz(int lin, int col) {
+    char **m = (char **) malloc(lin * sizeof(char *));
+    for (int i = 0; i < lin; i++) {
+        m[i] = (char *) malloc(col * sizeof(char));
+        for (int j = 0; j < col; j++)
+            m[i][j] = ' ';
+    }
+    return m;
+}
+void imprimeMat(char **mat, int lin, int col) {
+    for (int i = 0; i < lin; i++) {
+        for (int j = 0; j < col; j++)
+            printf("%c ", mat[i][j]);
+        printf("\n");
+    }
+}
+
+Pasta *constrArv(char **inOrdem, char **preOrdem, int l, int r, int *indPre, Pasta *pai, Filho filho) {
+    if (l < r) {
+        char prog[30];
+        strcpy(prog, preOrdem[(*indPre)++]);
+        int i = l;
+        while (strcmp(prog, inOrdem[i]))
+            i++;
+        Pasta *novo = malloc(sizeof(Pasta));
+        strcpy(novo->programa, prog);
+        novo->pai = pai;//Atualizei mas ainda não testei, se der merda coloque essa linha dentro de if(pai)
+        if (pai) {
+            strcpy(novo->nome, pai->programa);
+            if (filho == Esq)
+                strcat(novo->nome, "_esq");
+            if (filho == Dir)
+                strcat(novo->nome, "_dir");
+        } else
+            strcpy(novo->nome, "raiz");
+        novo->esq = constrArv(inOrdem, preOrdem, l, i, indPre, novo, Esq);
+        novo->dir = constrArv(inOrdem, preOrdem, i + 1, r, indPre, novo, Dir);
+        return novo;
+    } else
+        return NULL;
+}
+
+int nivelPrograma(Pasta *raiz, char *busca, int nivel) {
+    if (raiz == NULL)
+        return -1;
+    if (!strcmp(raiz->programa, busca))
+        return nivel;
+    int esquerda = nivelPrograma(raiz->esq, busca, nivel + 1);
+    int direita = nivelPrograma(raiz->dir, busca, nivel + 1);
+
+    if (esquerda == -1)
+        return direita;
+    else
+        return esquerda;
+}
+void testeVelocidade(int t_gasto, int t_max, char *prog) {
+    if (t_gasto > t_max)
+        printf("[DELAY][FAIL] O acesso ao programa %s.exe ultrapassou o limite de %d segundo\n", prog, t_max);
+    else
+        printf("[DELAY][OK] O acesso ao programa %s.exe foi concluido em %d segundos\n", prog, t_gasto);
+}
+
+Pasta* balanceamento(Pasta *raiz) {
+    int indice=0;
+    char **aux = alocaMatriz(NUM_PROG, 30);
+    extraiSemente(raiz, aux, &indice);
+    imprimeMat(aux, NUM_PROG, 30);
+    desaloca(raiz);
+    raiz = criaArv(aux, 0, NUM_PROG-1, Raiz, NULL);
+
+    imprimePreOrdem(raiz);
+    return raiz;
 }
 
 
-void imprime_Pre(No *arvore){
-  if(arvore){
-    if(arvore->estado == Livre)
-      printf("(L:%d)", arvore->size_max);
-    else if(arvore->estado == Particionado)
-      printf("(P:%d)", arvore->size_max);
-      else
-	printf("(O:%d/%d[%d])", arvore->size, arvore->size_max, arvore->cod);   
-    imprime_Pre(arvore->esq);
-    imprime_Pre(arvore->dir);
-  }
+void extraiSemente(Pasta *raiz, char **aux, int *indice){
+    if(raiz){
+        //strcpy(aux[(*indice)++], raiz->programa);
+        extraiSemente(raiz->esq, aux, indice);
+        strcpy(aux[(*indice)++], raiz->programa);
+        extraiSemente(raiz->dir, aux, indice);
+        //strcpy(aux[(*indice)++], raiz->programa);
+    }
 }
 
+Pasta *criaArv(char **semente, int l, int r, Filho filho, Pasta *pai){
+    if (r > l) {
+        int meio = floor((r - l) / 2);
+        Pasta *novo = malloc(sizeof(Pasta));
+        strcpy(novo->programa, semente[meio]);
+        novo->pai = pai;
+        if (pai) {
+            strcpy(novo->nome, pai->programa);
+            if (filho == Esq)
+                strcat(novo->nome, "_esq");
+            if (filho == Dir)
+                strcat(novo->nome, "_dir");
+        } else
+            strcpy(novo->nome, "raiz");
 
-
-void imprime_Pos(No *arvore){
-  if(arvore){
-    imprime_Pos(arvore->esq);
-    imprime_Pos(arvore->dir);
-    if(arvore->estado == Livre)
-      printf("(L:%d)", arvore->size_max);
-    else if(arvore->estado == Particionado)
-      printf("(P:%d)", arvore->size_max);
-      else
-	printf("(O:%d/%d[%d])", arvore->size, arvore->size_max, arvore->cod);  
-    
-  }
+        novo->esq = criaArv(semente, l, meio-1, Esq, novo);
+        novo->dir = criaArv(semente, meio+1, r, Dir, novo);
+        return novo;
+    }
+    else if(l == r) {
+        Pasta *novo = malloc(sizeof(Pasta));
+        strcpy(novo->programa, semente[r]);
+        novo->pai = pai;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        if (pai) {
+            strcpy(novo->nome, pai->programa);
+            if (filho == Esq)
+                strcat(novo->nome, "_esq");
+            if (filho == Dir)
+                strcat(novo->nome, "_dir");
+        } else
+            strcpy(novo->nome, "raiz");
+        return novo;
+    }
+    else
+        return NULL;
 }
 
-void desalocar(No *arvore){  
-  if(arvore){
-    desalocar(arvore->esq);
-    desalocar(arvore->dir);
-    free(arvore);
-  }
-}  
+void desaloca(Pasta *raiz) {
+    if (raiz) {
+        desaloca(raiz->esq);
+        desaloca(raiz->dir);
+        free(raiz);
+    }
+}
