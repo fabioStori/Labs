410a5b35b79b565686aae94f8541da9506710f40
Fabio Stori <33457554+fabioStori@users.noreply.github.com>
2017-11-21 14:54:54 -0200

    Update Lab08.c

--- Lab08.c
+++ Lab08.c
@@ -38,25 +38,27 @@ int main() {
     char ilha1[30], ilha2[30];
 
     scanf("%d %d %d", &limRed, &limBlue, &L);
-    IlhasGreen *ilhasGreen;
+    IlhasGreen *ilhasGreen = malloc(sizeof(IlhasGreen*));
 
-    for (i = 0; i < L; i++) {
-        //ilhasGreen[i] = malloc(sizeof(IlhasGreen));
+    for (i = 0; i < L; i++) {        
         scanf("%s %d", ilhasGreen[i].nome, &ilhasGreen[i].custo);
     }
+    
     Grafo g;
     int quantIlhas = L+2;
-    printf("segmentation fault\n");
     iniciaGrafo(&g, quantIlhas, ilhasGreen);
-
+    
     while (scanf("%s %s %d", ilha1, ilha2, &distancia) != EOF) {
         if(!(strcmp(ilha1, "Red"))){
+            printf("red\n");
             insereAresta(g, 0, achaIndice(g, ilha2, quantIlhas), distancia);
             //insere aresta da ilha green ligada ao red
         }else if(!(strcmp(ilha1, "Blue"))){
+            printf("blue\n");
             insereAresta(g, 1, achaIndice(g, ilha2, quantIlhas), distancia);
             //insere aresta da ilha green ligada ao blue
         }else{
+            printf("green\n");
             insereAresta(g, achaIndice(g, ilha1, quantIlhas), achaIndice(g, ilha2, quantIlhas), distancia);
             //insere aresta da ilha green liga a outra green
         }
@@ -69,55 +71,68 @@ int main() {
 }
 
 void iniciaGrafo(Grafo *g, int n, IlhasGreen *ilhasGreen) {
-    int i;
+    int i, j=0;
+    //printf("ilhagreen: %s\n", ilhasGreen[1].nome);
+    //char nome[30]; strcpy(nome, ilhasGreen[1].nome);
     g->quantIlhas = n;
     g->adj = malloc(n * sizeof(No *));
     for (i = 0; i < n; i++){
+        g->adj[i] = malloc(sizeof(No));
         g->adj[i]->prox = NULL;
         g->adj[i]->distancia = -1;
-        if(i==0)
-            strcpy(g->adj[i]->nome, "Red");
-        else if(i==1)
-            strcpy(g->adj[i]->nome, "Blue");
-        else{
-            strcpy(g->adj[i]->nome, ilhasGreen[i].nome);
+        if(i==0){
+            strcpy(g->adj[i]->nome, "Red");  strcpy(g->adj[i]->nome, "Red");
+            printf("red %s\n", g->adj[i]->nome);
+        }else if(i==1){
+            strcpy(g->adj[i]->nome, "Blue"); strcpy(g->adj[i]->nome, "Blue");
+            printf("blue %s\n", g->adj[i]->nome);
+        }else{
+            strcpy(g->adj[i]->nome, ilhasGreen[i-2].nome); strcpy(g->adj[i]->nome, ilhasGreen[i-2].nome);
+            printf("green %s\n", g->adj[i]->nome);
         }
     }
 }
 
 No * insereNaLista(No *lista , int vert, char nome[30], int distancia) {
     No *novo = malloc(sizeof(No));
-    strcpy(novo->nome, nome);
     novo->distancia = distancia;
-    novo ->prox = lista;
+    strcpy(novo->nome, nome);
+    novo->prox = lista;
     return novo;
 }
 
 void insereAresta(Grafo g, int i, int j, int distancia) {
+    printf("i: %d j: %d\n", i, j);
     g.adj[i] = insereNaLista(g.adj[i], j, g.adj[j]->nome, distancia);
-    g.adj[j] = insereNaLista(g.adj[j], i, g.adj[i]->nome, distancia);
+    //g.adj[j] = insereNaLista(g.adj[j], i, g.adj[i]->nome, distancia);
 }
 
 int achaIndice(Grafo g, char nome[30], int L){
-    int i;
+    int i; No *tmp;
     for(i=0;i<L;i++){
-        if(strcmp(g.adj[i]->nome, nome)==0)
+        tmp=g.adj[i];
+        while(tmp->prox!=NULL)
+            tmp=tmp->prox;
+        if(strcmp(tmp->nome, nome)==0){
+            printf("nome indice %s\n", tmp->nome);
             return i;
+        }
     }
+    return -1;
 }
 
 void imprimir(Grafo g, int L){
     int i;
     for(i=0;i<L;i++){
         if(g.adj[i]->prox!=NULL){
-            No* tmp = g.adj[i]->prox;
+            No* tmp = g.adj[i];
             while(tmp!=NULL){
-                printf("indice [%d] nome [%s]", i, tmp->nome);
+                printf("indice [%d] nome [%s] distancia [%d]\n", i, tmp->nome, tmp->distancia);
                 tmp=tmp->prox;
             }
         }else{
-            printf("indice [%d] nome [%s]", i, g.adj[i]->nome);
-        }
+            printf("indice [%d] nome [%s] distancia [%d]\n", i, g.adj[i]->nome, g.adj[i]->distancia);
+        }printf("\n");
     }
 }
 /*
